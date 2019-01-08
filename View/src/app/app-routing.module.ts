7ad92f7e6c7c972134b02d103db59b48f1ad2d2c
mk1808 <monikor97@gmail.com>
2019-01-08 16:24:01 +0100

    #1 navbar, poczatek listy

--- View/src/app/app-routing.module.ts
+++ View/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './list/list.component';
+import { ListViewComponent } from './list-view/list-view.component';
 
 const routes: Routes = [
-  { path: 'list', component: ListComponent },
+  { path: 'list', component: ListViewComponent },
 ];
 
 @NgModule({
--- View/src/app/app.module.ts
+++ View/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LayoutComponent } from './layout/layout.component';
+import { ListViewComponent } from './list-view/list-view.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LayoutComponent,
+    ListViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
