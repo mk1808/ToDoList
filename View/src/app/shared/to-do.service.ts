5740389743c61fa38648a014065d4c0df14255be
mk1808 <monikor97@gmail.com>
2019-01-11 15:45:18 +0100

    #1 stworzenie seriwsow

--- View/src/app/shared/to-do.service.ts
+++ View/src/app/shared/to-do.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Task, List } from './classes';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToDoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public createTask(task:Task): Observable<any> {
+    return this.http.post<any>('http://localhost/todo/controllers/createTask.php',
+    JSON.stringify(task));
+}
+public createList(list:List): Observable<any> {
+  return this.http.post<any>('http://localhost/todo/controllers/createList.php',
+  JSON.stringify(list));
+}
+
+public deleteTask(id): Observable<any> {
+  return this.http.post<any>('http://localhost/todo/controllers/deleteTask.php',
+  JSON.stringify(id));
+}
+
+public deleteList(id): Observable<any> {
+  return this.http.post<any>('http://localhost/todo/controllers/deleteList.php',
+  JSON.stringify(id));
+}
+
+public getLists(): Observable<List[]> {
+  
+  return this.http.get<List[]>('http://localhost/todo/controllers/getLists.php');
+}
+
+
+public getTasksForList(id): Observable<any> {
+  return this.http.get<any>('http://localhost/todo/controllers/getTasksForList.php?id=',id);
+
+}
 }
